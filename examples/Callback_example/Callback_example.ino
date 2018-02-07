8098cd7c0198c7e54a4a459e35f98fe9df456401
Species5618 <github@species5618.net>
2018-02-07 21:56:08 +0000

    fixed typo in cpp file fixed minor include typo in call back example fixed minor include typo in StartHere example
    both example now compile without issue and test on ESP01 will test on wemos in due course

--- examples/Callback_example/Callback_example.ino
+++ examples/Callback_example/Callback_example.ino
@@ -1,5 +1,5 @@
 #include <ESP8266WiFi.h>
-#include "FS.h"
+#include <FS.h>
 #include <WiFiClient.h>
 #include <TimeLib.h>
 #include <NtpClientLib.h>
@@ -9,8 +9,9 @@
 #include <Ticker.h>
 #include <ArduinoOTA.h>
 #include <ArduinoJson.h>
-#include "FSWebServerLib.h"
+#include <FSWebServerLib.h>
 #include <Hash.h>
+
 
 
 void  callbackJSON(AsyncWebServerRequest *request)
